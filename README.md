df77dd99e6c0cc40aa6aeba220d050126fa26ee1
abc <>
2021-09-18 22:19:18 -0400

    time period testing and dynamic interval

--- README.md
+++ README.md
@@ -22,7 +22,23 @@ To run the simple unit tests:
 yarn test
 ```
 
-To show and hide content at a fixed interval, wrap a component in the `WithPeekaboo` function as demonstrated in `src/components/the-subtitle`. Pass an interval in milliseconds.
+To show and hide content at a fixed interval, wrap a component in the `WithPeekaboo` function as demonstrated in `src/components/the-subtitle`.
 
 ## how it works
-with-peekaboo is the component. It and its corresponding styles can be found in `src/components/with-peekaboo`.
+with-peekaboo is the component. It and its corresponding styles can be found in `src/components/with-peekaboo`. It uses the higher-order component (HOC) pattern
+
+## why did I use an HOC pattern?
+Because the functionality pertains to showing and hiding parts of the DOM...this is a DOM-centric feature. An HOC is intended to wrap a component and this functionality will always be applied to components. You have the flexibility of creating something like a `div`, `span` or whatever you like that is wrapped by `WithPeekaboo`. All of that DOM element's children will then flash hidden or visible. 
+
+At the moment, I see Hooks as a good way to add data or other attributes to a component. But I don't see it as a good way to add features or functionality to a component. HOC's seem better suited for that purpose. By the same token, `useState` seems clearer and more idiomatic to me than `useEffect`. 
+
+Finally, there was a choice of using hooks in this assignment so I chose not to. If there were no choice in the matter, I would have used a hook.
+
+## considerations regarding application state
+We want the `interval` prop to be dynamically updatable by the user. Because an HOC pattern is used for WithPeekaboo, we really want to be able to wrap any component in the DOM without having to think too much about how to pass down the `interval` prop to `WithPeekaboo`. I.e, we really want to avoid prop drilling and we also want code maintenance to be easy. Hence, a state library was chosen to lift `interval` to a global state variable and why Context API was rejected. Redux was chosen simply because it is the most common state management library for React.
+
+## a note on the tests
+I probably don't have enough tests. I just wanted to demonstrate how I would set them up and have some basic testing complete.
+
+## final note on the interval input boxes
+I did not try to style these at all! I skipped that aspect entirely because it is just a means to an end (setting the interval and showing that it is able to be dynamically set).
--- src/components/with-peekaboo/with-peekaboo.test.js
+++ src/components/with-peekaboo/with-peekaboo.test.js
@@ -1,23 +1,45 @@
 import { render, screen } from '@testing-library/react';
 import WithPeekaboo from './with-peekaboo.component';
 
-test('renders an empty span', () => {
-    const PeekabooDiv = WithPeekaboo();
-    const { container } = render(<PeekabooDiv/>);
-    const peakabooElement = container.querySelector('.peekaboo');
-    expect(peakabooElement).toBeInTheDocument();
-    const peakabooChildElements = container.querySelector('.peekaboo *');
-    expect(peakabooChildElements).toBeNull();
-});
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import intervalReducer from '../../redux/intervalSlice';
 
-test('wrapped component is displayed with all props', () => {
-    const emptyDiv = () => {
-        return <div prop1='val1' prop2='val2'/>;
-    }
-    const PeekabooDiv = WithPeekaboo(emptyDiv);
-    const { container } = render(<PeekabooDiv/>);
-    const wrappedByProp1 = container.querySelector('[prop1="val1"]');
-    expect(wrappedByProp1).toBeInTheDocument();
-    const wrappedByProp2 = container.querySelector('[prop2="val2"]');
-    expect(wrappedByProp2).toBeInTheDocument();
+const store = createStore(combineReducers({
+    interval: intervalReducer,
+}))
+
+jest.useFakeTimers();
+
+describe('peekaboo', () => {
+
+    test('renders an empty span', () => {
+        const PeekabooDiv = WithPeekaboo();
+        const { container } = render(<Provider store={store}><PeekabooDiv/></Provider>);
+        const peakabooElement = container.querySelector('.peekaboo');
+        expect(peakabooElement).toBeInTheDocument();
+        const peakabooChildElements = container.querySelector('.peekaboo *');
+        expect(peakabooChildElements).toBeNull();
+    });
+    
+    test('wraps a component which is displayed with all props', () => {
+        const sampleDiv = () => {
+            return <div prop1='val1' prop2='val2'/>;
+        }
+        const PeekabooDiv = WithPeekaboo(sampleDiv);
+        const { container } = render(<Provider store={store}><PeekabooDiv/></Provider>);
+        const wrappedByProp1 = container.querySelector('[prop1="val1"]');
+        expect(wrappedByProp1).toBeInTheDocument();
+        const wrappedByProp2 = container.querySelector('[prop2="val2"]');
+        expect(wrappedByProp2).toBeInTheDocument();
+    });
+
+    test('shows and hides with the correct time period', () => {
+        const sampleDiv = () => {
+            return <div prop1='val1' prop2='val2'/>;
+        }
+        const PeekabooDiv = WithPeekaboo(sampleDiv);
+        const { container } = render(<Provider store={store}><PeekabooDiv/></Provider>);
+        expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+    });
 });
